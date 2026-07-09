fb3498705c31f0b99be165b070a6a0c61274aadb
Harish Kumar <harya0102@gmail.com>
2026-07-09 13:29:50 +0530

    Time: 8 ms (80.27%), Space: 224.1 MB (78.36%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff,
-                                      vector<vector<int>>& queries) {
-
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
         vector<int> group(n);
-        group[0] = 0;
-
-        for (int i = 1; i < n; i++) {
-            if (nums[i] - nums[i - 1] <= maxDiff)
-                group[i] = group[i - 1];
-            else
-                group[i] = group[i - 1] + 1;
+        group[0]=0;
+        for(int i=1;i<n;i++){
+            if(abs(nums[i]-nums[i-1])<=maxDiff){
+                group[i]=group[i-1];
+            }
+            else{
+                group[i]=group[i-1]+1;
+            }
         }
-
         vector<bool> ans;
-
-        for (auto &q : queries) {
-            ans.push_back(group[q[0]] == group[q[1]]);
+        for(auto &q:queries){
+            ans.push_back(group[q[0]]==group[q[1]]);
         }
-
         return ans;
     }
 };
