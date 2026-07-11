3b48d5e7ebf440cf9d2bfd18f04094ade3076d9d
Harish Kumar <harya0102@gmail.com>
2026-07-12 01:01:30 +0530

    Time: 0 ms (100%), Space: 10.8 MB (5.55%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,63 +1,49 @@
-#include<bits/stdc++.h>
 class Solution {
 public:
-    bool isChar(char ch){
-        
-           if ((ch>='A' && ch<='Z') || (ch>='a' && ch<='z') || (ch>='0' && ch<='9')){
+    bool isValid(char ch){
+        if(ch>='a' && ch<='z' || ch>='A' && ch<='Z' || ch>='0' && ch<='9'){
             return 1;
-           } 
-           else{
-            return 0;
-           }
-        
+        }
+        return 0;
     }
     char isLower(char ch){
-        
-        //    char ch=s[i];
         if(ch>='a' && ch<='z'){
             return ch;
         }
-        if(ch>='A' && ch<='Z'){
-            // char temp=ch;
+        else if(ch>='A' && ch<='Z'){
             return ch-'A'+'a';
-            
         }
         return ch;
     }
-    bool checkP(string s){
+    bool checkPalindrome(string s){
         int st=0;
         int e=s.length()-1;
         while(st<=e){
-            if(s[st]!=s[e]){
-                return 0;
-            }
+            if(s[st]!=s[e]) return 0;
             else{
-                st++;
                 e--;
-                
-
+                st++;
             }
             
         }
         return 1;
+        
+            
+        
+        
     }
     bool isPalindrome(string s) {
         string str="";
         for(int i=0;i<s.length();i++){
             char ch=s[i];
-            if(isChar(ch)){
+            if(isValid(ch)){
                 str.push_back(ch);
             }
-        
-
         }
         for(int i=0;i<str.length();i++){
-                char ch=str[i];
-                str[i]=isLower(ch);
-            
+            str[i]=isLower(str[i]);
         }
-        return checkP(str);
-
+        return checkPalindrome(str);
         
     }
 };
