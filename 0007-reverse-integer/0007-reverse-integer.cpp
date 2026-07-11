98474da4ddb88d698d8e11540cfeff886a35a182
Harish Kumar <harya0102@gmail.com>
2026-07-12 00:19:24 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.97%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -4,16 +4,13 @@ public:
         int rev=0;
         while(x!=0){
             int digit=x%10;
-            
-            
-            if ((rev > INT_MAX/10) || (rev < INT_MIN/10)){
+            if((rev>INT_MAX/10 )|| (rev<INT_MIN/10)){
                 return 0;
-
             }
-            x=x/10;
+            x/=10;
             rev=(rev*10)+digit;
             
         }
-        return rev;  
+        return rev;
     }
 };
