ecce9af61c8928ebebfc331caef983aa7fa1de21
Harish Kumar <harya0102@gmail.com>
2026-07-16 02:28:17 +0530

    Time: 3 ms (7.56%), Space: 11 MB (94.73%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> ans;
+        for(int i=0;i<heights.size();i++){
+            ans.push_back(heights[i]);
+        }
+        sort(ans.begin(),ans.end());
+        int count=0;
+        for(int i=0;i<ans.size();i++){
+            if(ans[i]!=heights[i]){
+                count++;
+            }
+        }
+        return count;
+    }
+};
