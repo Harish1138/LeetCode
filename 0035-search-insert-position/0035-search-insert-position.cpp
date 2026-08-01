08ab3c0f67ffabafd1042a6c8653cabe26e12a65
Harish Kumar <harya0102@gmail.com>
2026-08-01 10:15:53 +0530

    Time: 0 ms (100%), Space: 13.5 MB (98.79%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        bool found=false;
-        int index;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){
-                index=i;
-                found=true;
-                break;
-            }
-            
-        }
-        if(!found){
-            for(int i=0;i<nums.size();i++){
-                if(nums[i]>target){
-                    index=i;
-                    break;
-                }
-            }
-        }
-        return index;
-        
+        return lower_bound(nums.begin(),nums.end(),target)-nums.begin();
     }
 };
