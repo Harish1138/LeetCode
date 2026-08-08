dc3166efba5882468abfcf7686dedcc16a53a1e5
Harish Kumar <harya0102@gmail.com>
2026-08-08 19:24:11 +0530

    Time: 45 ms (55.32%), Space: 103.9 MB (62.77%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.cpp
@@ -4,30 +4,32 @@ public:
         int n=word1.length();
         int m=word2.length();
         int j=m-1;
-        vector<int> last (m,-1);
+        vector<int> last(m,-1);
         for(int i=n-1;i>=0;i--){
             if(j>=0 && word1[i]==word2[j]){
                 last[j]=i;
                 j--;
             }
         }
+        int k=0;
         vector<int> res;
         int skip=0;
-        j=0;
         for(int i=0;i<n;i++){
-            if(j==m) break;
-            if((word1[i]==word2[j])|| (skip==0 && (j==m-1 ||i<last[j+1]))){
-                res.push_back(i);
-                // j++;
-                if(word1[i]!=word2[j]){
-                    skip+=1;
-                }
-                j++;
-                
+            if(k==m){
+                break;
             }
-
+            if((word1[i]==word2[k]) || (skip==0 && (k==m-1 || i<last[k+1]))){
+                res.push_back(i);
+                if(word1[i]!=word2[k]){
+                    skip++;
+                }
+                else{
+                    skip+=0;
+                }
+                k++;
+            }
         }
-        if(j==m){
+        if(k==m){
             return res;
         }
         return vector<int>();
