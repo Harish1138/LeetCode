d56903893a19d73bc0d1fd79a785df28615c9b37
Harish Kumar <harya0102@gmail.com>
2026-08-10 05:11:01 +0530

    Time: 39 ms (61.65%), Space: 85.8 MB (92.72%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -4,6 +4,9 @@ public:
         sort(nums.begin(),nums.end(),greater<int>());
         int maxDiff=0;
         int j=1;
+        if(nums.size()<2){
+            return maxDiff;
+        }
         for(int i=0;i<nums.size();i++){
             if(j>=nums.size()) break;
             int min=abs(nums[i]-nums[j++]);
