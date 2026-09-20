c028c80e481e47f695b51a8f0e926b18126f9c0d
Harish Kumar <harya0102@gmail.com>
2026-09-20 15:04:59 +0530

    Time: 0 ms (100%), Space: 9.8 MB (47.88%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int reverseDegree(string s) {
-        unordered_map<char,int> mpp;
-        char ch='a';
-        for(int i=26;i>=1;i--){
-            mpp[ch++]=i;
-        }
-        long long sum=0;
-        int j=1;
-        for(int i=0;i<s.length();i++){
-            char ch=s[i];
-            for(auto it:mpp){
-                if(ch==it.first){
-                    sum+=j++*(it.second);
-                }
-            }
 
+        int i=1;
+        long long rev=0;
+        for(int j=0;j<s.length();j++){
+            int sum = 'z'-s[j]+1;
+            rev+=i++*sum;
+            
         }
-        return sum;
+        return rev;
         
     }
 };
