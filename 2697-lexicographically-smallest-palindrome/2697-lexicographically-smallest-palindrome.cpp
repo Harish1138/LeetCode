4711bae1658f9593dee5c592274ae545c30327c2
Harish Kumar <harya0102@gmail.com>
2026-09-22 21:38:22 +0530

    Time: 4 ms (63.05%), Space: 18.6 MB (24.83%) - LeetHub

---
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string makeSmallestPalindrome(string s) {
+        int low=0;
+        int high=s.length()-1;
+        int mid=low+(high-low)/2;
+        while(low<=high){
+            if(s[low]!=s[high]){
+                if((int)s[low]<(int)s[high]){
+                    s[high]=s[low];
+                }
+                else{
+                    s[low]=s[high];
+                }
+                
+            }
+            low++;
+            high--;
+
+        }
+        return s;
+        
+    }
+};
